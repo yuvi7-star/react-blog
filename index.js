aa4e16c72d5bb7e2b1e2a6854a89ef8ad9acb41b
yuvi <yuvrajsarvaiya8@gmail.com>
2020-04-23 18:00:38 +0530

    added mongo key

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./model/user');
+
+const config = require('./config/key');
 
 const app = express();
 
-mongoose
-  .connect(
-    'mongodb+srv://mongoyuvi:13011999@react-blog-cluster-zvqzc.mongodb.net/test?retryWrites=true&w=majority',
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log('connected'))
-  .catch(() => console.log('error while connecting to db') );
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.use('/', (req, res) => {
-  res.send('hello');
+app.post('/api/user/register', (req, res, next) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({ success: true });
+  });
 });
 
 const PORT = 4000;
 
-app.listen(PORT, () => {
-  console.log('server started');
-});
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to databse');
+    app.listen(PORT, () => {
+      console.log('server started');
+    });
+  })
+  .catch(() => console.log('error while connecting to db'));
